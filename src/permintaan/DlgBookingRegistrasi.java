888ddb54743c5d2e36aac2e0b257e83d58d74b3e
aleealee123 <adlyhidayat@gmail.com>
2022-12-20 11:26:03 +0800

    WA BATAL

--- src/permintaan/DlgBookingRegistrasi.java
+++ src/permintaan/DlgBookingRegistrasi.java
@@ -56,6 +56,10 @@ public class DlgBookingRegistrasi extends javax.swing.JDialog {
     private DlgCariDokter2 dokter2=new DlgCariDokter2(null,false);
     private DlgCariPoli poli=new DlgCariPoli(null,false);
     private DlgCariPoli2 poli2=new DlgCariPoli2(null,false);
+    private DlgCariPoli poli3=new DlgCariPoli(null,false);
+    private DlgCariPoli poli4=new DlgCariPoli(null,false);
+    private DlgCariPoli2 poli5=new DlgCariPoli2(null,false);
+    private DlgCariPoli2 poli6=new DlgCariPoli2(null,false);
     private DlgPasien pasien=new DlgPasien(null,false);
     private String aktifjadwal="",URUTNOREG="",status="",no_rawat="",umur="",sttsumur="";
     private JsonNode root, nameNode ,response;
@@ -72,7 +76,7 @@ public class DlgBookingRegistrasi extends javax.swing.JDialog {
     public DlgBookingRegistrasi(java.awt.Frame parent, boolean modal) {
         super(parent, modal);
         initComponents();
-        WindowBatalWhatsapp.setSize(410, 115);
+        WindowBatalWhatsapp.setSize(500, 144);
 
         tabMode=new DefaultTableModel(null,new Object[]{
                 "P","Tgl.Booking","Jam Booking","No.RM","Nama Pasien","Tgl.Periksa","Kode Dokter",
@@ -306,6 +310,94 @@ public class DlgBookingRegistrasi extends javax.swing.JDialog {
             public void windowDeactivated(WindowEvent e) {}
         }); 
         
+        poli3.addWindowListener(new WindowListener() {
+            @Override
+            public void windowOpened(WindowEvent e) {}
+            @Override
+            public void windowClosing(WindowEvent e) {}
+            @Override
+            public void windowClosed(WindowEvent e) {
+                if(poli3.getTable().getSelectedRow()!= -1){                    
+                    KdPoli1.setText(poli3.getTable().getValueAt(poli3.getTable().getSelectedRow(),0).toString());
+                    NmPoli1.setText(poli3.getTable().getValueAt(poli3.getTable().getSelectedRow(),1).toString());
+                }   
+            }
+            @Override
+            public void windowIconified(WindowEvent e) {}
+            @Override
+            public void windowDeiconified(WindowEvent e) {}
+            @Override
+            public void windowActivated(WindowEvent e) {}
+            @Override
+            public void windowDeactivated(WindowEvent e) {}
+        });
+        
+        poli5.addWindowListener(new WindowListener() {
+            @Override
+            public void windowOpened(WindowEvent e) {}
+            @Override
+            public void windowClosing(WindowEvent e) {}
+            @Override
+            public void windowClosed(WindowEvent e) {
+                if(poli5.getTable().getSelectedRow()!= -1){                    
+                    KdPoli1.setText(poli5.getTable().getValueAt(poli5.getTable().getSelectedRow(),0).toString());
+                    NmPoli1.setText(poli5.getTable().getValueAt(poli5.getTable().getSelectedRow(),1).toString());
+                }    
+            }
+            @Override
+            public void windowIconified(WindowEvent e) {}
+            @Override
+            public void windowDeiconified(WindowEvent e) {}
+            @Override
+            public void windowActivated(WindowEvent e) {}
+            @Override
+            public void windowDeactivated(WindowEvent e) {}
+        });
+        
+        poli4.addWindowListener(new WindowListener() {
+            @Override
+            public void windowOpened(WindowEvent e) {}
+            @Override
+            public void windowClosing(WindowEvent e) {}
+            @Override
+            public void windowClosed(WindowEvent e) {
+                if(poli4.getTable().getSelectedRow()!= -1){                    
+                    KdPoli2.setText(poli4.getTable().getValueAt(poli4.getTable().getSelectedRow(),0).toString());
+                    NmPoli2.setText(poli4.getTable().getValueAt(poli4.getTable().getSelectedRow(),1).toString());
+                }   
+            }
+            @Override
+            public void windowIconified(WindowEvent e) {}
+            @Override
+            public void windowDeiconified(WindowEvent e) {}
+            @Override
+            public void windowActivated(WindowEvent e) {}
+            @Override
+            public void windowDeactivated(WindowEvent e) {}
+        });
+        
+        poli6.addWindowListener(new WindowListener() {
+            @Override
+            public void windowOpened(WindowEvent e) {}
+            @Override
+            public void windowClosing(WindowEvent e) {}
+            @Override
+            public void windowClosed(WindowEvent e) {
+                if(poli6.getTable().getSelectedRow()!= -1){                    
+                    KdPoli2.setText(poli6.getTable().getValueAt(poli6.getTable().getSelectedRow(),0).toString());
+                    NmPoli2.setText(poli6.getTable().getValueAt(poli6.getTable().getSelectedRow(),1).toString());
+                }    
+            }
+            @Override
+            public void windowIconified(WindowEvent e) {}
+            @Override
+            public void windowDeiconified(WindowEvent e) {}
+            @Override
+            public void windowActivated(WindowEvent e) {}
+            @Override
+            public void windowDeactivated(WindowEvent e) {}
+        });
+        
         pasien.addWindowListener(new WindowListener() {
             @Override
             public void windowOpened(WindowEvent e) {}
@@ -533,9 +625,9 @@ public class DlgBookingRegistrasi extends javax.swing.JDialog {
         internalFrame7.add(BtnCloseIn6);
         BtnCloseIn6.setBounds(310, 100, 100, 30);
 
-        BtnSimpan6.setIcon(new javax.swing.ImageIcon(getClass().getResource("/picture/save-16x16.png"))); // NOI18N
+        BtnSimpan6.setIcon(new javax.swing.ImageIcon(getClass().getResource("/picture/delete-16x16.png"))); // NOI18N
         BtnSimpan6.setMnemonic('S');
-        BtnSimpan6.setText("Simpan");
+        BtnSimpan6.setText("Kirim WA Batal");
         BtnSimpan6.setToolTipText("Alt+S");
         BtnSimpan6.setName("BtnSimpan6"); // NOI18N
         BtnSimpan6.addActionListener(new java.awt.event.ActionListener() {
@@ -544,7 +636,7 @@ public class DlgBookingRegistrasi extends javax.swing.JDialog {
             }
         });
         internalFrame7.add(BtnSimpan6);
-        BtnSimpan6.setBounds(40, 100, 100, 30);
+        BtnSimpan6.setBounds(40, 100, 150, 30);
 
         jLabel12.setText("Dari Unit/Poli :");
         jLabel12.setName("jLabel12"); // NOI18N
@@ -886,7 +978,7 @@ public class DlgBookingRegistrasi extends javax.swing.JDialog {
         R2.setPreferredSize(new java.awt.Dimension(125, 23));
         panelCari.add(R2);
 
-        DTPCari1.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "05-10-2022" }));
+        DTPCari1.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "12-12-2022" }));
         DTPCari1.setDisplayFormat("dd-MM-yyyy");
         DTPCari1.setName("DTPCari1"); // NOI18N
         DTPCari1.setOpaque(false);
@@ -909,7 +1001,7 @@ public class DlgBookingRegistrasi extends javax.swing.JDialog {
         jLabel22.setPreferredSize(new java.awt.Dimension(25, 23));
         panelCari.add(jLabel22);
 
-        DTPCari2.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "05-10-2022" }));
+        DTPCari2.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "12-12-2022" }));
         DTPCari2.setDisplayFormat("dd-MM-yyyy");
         DTPCari2.setName("DTPCari2"); // NOI18N
         DTPCari2.setOpaque(false);
@@ -931,7 +1023,7 @@ public class DlgBookingRegistrasi extends javax.swing.JDialog {
         R3.setPreferredSize(new java.awt.Dimension(135, 23));
         panelCari.add(R3);
 
-        DTPCari3.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "05-10-2022" }));
+        DTPCari3.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "12-12-2022" }));
         DTPCari3.setDisplayFormat("dd-MM-yyyy");
         DTPCari3.setName("DTPCari3"); // NOI18N
         DTPCari3.setOpaque(false);
@@ -954,7 +1046,7 @@ public class DlgBookingRegistrasi extends javax.swing.JDialog {
         jLabel25.setPreferredSize(new java.awt.Dimension(25, 23));
         panelCari.add(jLabel25);
 
-        DTPCari4.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "05-10-2022" }));
+        DTPCari4.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "12-12-2022" }));
         DTPCari4.setDisplayFormat("dd-MM-yyyy");
         DTPCari4.setName("DTPCari4"); // NOI18N
         DTPCari4.setOpaque(false);
@@ -1038,7 +1130,7 @@ public class DlgBookingRegistrasi extends javax.swing.JDialog {
         TPasien.setBounds(151, 10, 311, 23);
 
         TanggalBooking.setForeground(new java.awt.Color(50, 70, 50));
-        TanggalBooking.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "05-10-2022 08:24:59" }));
+        TanggalBooking.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "12-12-2022 01:06:24" }));
         TanggalBooking.setDisplayFormat("dd-MM-yyyy hh:mm:ss");
         TanggalBooking.setName("TanggalBooking"); // NOI18N
         TanggalBooking.setOpaque(false);
@@ -1118,7 +1210,7 @@ public class DlgBookingRegistrasi extends javax.swing.JDialog {
         jLabel14.setBounds(506, 40, 70, 23);
 
         TanggalPeriksa.setForeground(new java.awt.Color(50, 70, 50));
-        TanggalPeriksa.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "05-10-2022 08:24:59" }));
+        TanggalPeriksa.setModel(new javax.swing.DefaultComboBoxModel(new String[] { "12-12-2022 01:06:24" }));
         TanggalPeriksa.setDisplayFormat("dd-MM-yyyy hh:mm:ss");
         TanggalPeriksa.setName("TanggalPeriksa"); // NOI18N
         TanggalPeriksa.setOpaque(false);
@@ -1664,7 +1756,6 @@ private void ChkInputActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRS
             if(tbObat.getValueAt(i,0).toString().equals("true")){
                 WindowBatalWhatsapp.setLocationRelativeTo(internalFrame1);
                 WindowBatalWhatsapp.setVisible(true);
-                kirimwa.sendWaBatal(tbObat.getValueAt(i,3).toString(),tbObat.getValueAt(i,4).toString(),tbObat.getValueAt(i,5).toString(),tbObat.getValueAt(i,9).toString());
             }
         }
     }//GEN-LAST:event_BtnEdit2ActionPerformed
@@ -1678,11 +1769,34 @@ private void ChkInputActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRS
     }//GEN-LAST:event_BtnCloseIn6ActionPerformed
 
     private void BtnSimpan6ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BtnSimpan6ActionPerformed
-        
+        for(i=0;i<tbObat.getRowCount();i++){ 
+            if(tbObat.getValueAt(i,0).toString().equals("true")){
+                kirimwa.sendWaBatal(tbObat.getValueAt(i,3).toString(),tbObat.getValueAt(i,4).toString(),tbObat.getValueAt(i,5).toString(),NmPoli1.getText(),NmPoli2.getText());        
+            }
+        }
     }//GEN-LAST:event_BtnSimpan6ActionPerformed
 
     private void BtnPoli1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BtnPoli1ActionPerformed
-        // TODO add your handling code here:
+        if(aktifjadwal.equals("aktif")){
+            if(var.getkode().equals("Admin Utama")){
+                poli3.isCek();        
+                poli3.setSize(internalFrame1.getWidth()-20,internalFrame1.getHeight()-20);
+                poli3.setLocationRelativeTo(internalFrame1);
+                poli3.setVisible(true);
+            }else{
+                poli5.isCek();                     
+                poli5.SetHari(TanggalPeriksa.getDate());
+                poli5.tampil(); 
+                poli5.setSize(internalFrame1.getWidth()-20,internalFrame1.getHeight()-20);
+                poli5.setLocationRelativeTo(internalFrame1);
+                poli5.setVisible(true);
+            }                
+        }else{
+            poli3.isCek();        
+            poli3.setSize(internalFrame1.getWidth()-20,internalFrame1.getHeight()-20);
+            poli3.setLocationRelativeTo(internalFrame1);
+            poli3.setVisible(true);
+        }
     }//GEN-LAST:event_BtnPoli1ActionPerformed
 
     private void BtnPoli1KeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_BtnPoli1KeyPressed
@@ -1690,7 +1804,26 @@ private void ChkInputActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRS
     }//GEN-LAST:event_BtnPoli1KeyPressed
 
     private void BtnPoli2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BtnPoli2ActionPerformed
-        // TODO add your handling code here:
+        if(aktifjadwal.equals("aktif")){
+            if(var.getkode().equals("Admin Utama")){
+                poli4.isCek();        
+                poli4.setSize(internalFrame1.getWidth()-20,internalFrame1.getHeight()-20);
+                poli4.setLocationRelativeTo(internalFrame1);
+                poli4.setVisible(true);
+            }else{
+                poli6.isCek();                     
+                poli6.SetHari(TanggalPeriksa.getDate());
+                poli6.tampil(); 
+                poli6.setSize(internalFrame1.getWidth()-20,internalFrame1.getHeight()-20);
+                poli6.setLocationRelativeTo(internalFrame1);
+                poli6.setVisible(true);
+            }                
+        }else{
+            poli4.isCek();        
+            poli4.setSize(internalFrame1.getWidth()-20,internalFrame1.getHeight()-20);
+            poli4.setLocationRelativeTo(internalFrame1);
+            poli4.setVisible(true);
+        }
     }//GEN-LAST:event_BtnPoli2ActionPerformed
 
     private void BtnPoli2KeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_BtnPoli2KeyPressed
