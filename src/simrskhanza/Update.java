354726249f8f39d3e8be24a5fd76aa3de330992a
aleealee123 <adlyhidayat@gmail.com>
2023-03-04 15:21:40 +0800

    com

--- src/simrskhanza/Update.java
+++ src/simrskhanza/Update.java
@@ -4,6 +4,7 @@ import fungsi.koneksiDB;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Properties;
 /**
@@ -12,12 +13,26 @@ import java.util.Properties;
  */
 public class Update {
     private static String URL ;
+    public static String version , value;
+    private static final Properties prop = new Properties();
     
     public static String getLatestVersion() throws Exception
     {
+        try {
+            prop.loadFromXML(new FileInputStream("setting/database.xml"));
+
+        } catch (Exception e) {
+            System.out.println("Notif Setting : "+e);
+        }
         URL= koneksiDB.UrlAutoUpdate()+"/changelog.txt";
         String data = getData(URL);
-        return data.substring(data.indexOf("[version]")+9,data.indexOf("[/version]"));
+        version = prop.getProperty("VERSION");
+        if (!"201".equals(data)) {
+            value = data.substring(data.indexOf("[version]")+9,data.indexOf("[/version]"));
+        } else {
+            value = version;
+        }
+        return value;
     }
     public static String getWhatsNew() throws Exception
     {
@@ -25,20 +40,25 @@ public class Update {
         String data = getData(URL);
         return data.substring(data.indexOf("[history]")+9,data.indexOf("[/history]"));
     }
-    private static String getData(String address)throws Exception
+    private static String getData(String address) throws IOException
     {
-        URL url = new URL(address);
-        
-        InputStream html = null;
-
-        html = url.openStream();
-        
-        int c = 0;
         StringBuffer buffer = new StringBuffer("");
+        try{
+            URL url = new URL(address);
+        
+            InputStream html = null;
 
-        while(c != -1) {
-            c = html.read();
-            buffer.append((char)c);
+            html = url.openStream();
+
+            int c = 0;
+
+            while(c != -1) {
+                c = html.read();
+                buffer.append((char)c);
+            }
+            
+        } catch(MalformedURLException e){
+            buffer.append("201");
         }
         return buffer.toString();
     }
