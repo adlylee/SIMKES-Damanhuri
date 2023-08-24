e5315d81c8676fcb5b674d97df6d2d725b308528
adlylee <adlyhidayat@gmail.com>
2023-08-24 14:14:26 +0700

    rads

--- src/bridging/ApiOrthanc.java
+++ src/bridging/ApiOrthanc.java
@@ -257,11 +257,11 @@ public class ApiOrthanc {
     public String ambilPreview(String studies_id) throws NoSuchAlgorithmException, KeyManagementException
     {
         String url;
-        headers = new HttpHeaders();
-        headers.add("Accept", "image/jpeg");
-        requestEntity = new HttpEntity(requestJson,headers);
-        url = getRest().exchange(koneksiDB.URLORTHANC()+":"+koneksiDB.PORTORTHANC()+"/instances/"+studies_id+"/rendered?quality=20&height=60&width=40", HttpMethod.GET, requestEntity, String.class).getBody();
-//        url = ;
+//        headers = new HttpHeaders();
+//        headers.add("Accept", "image/jpeg");
+//        requestEntity = new HttpEntity(requestJson,headers);
+//        url = getRest().exchange(koneksiDB.URLORTHANC()+":"+koneksiDB.PORTORTHANC()+"/instances/"+studies_id+"/rendered?quality=20&height=60&width=40", HttpMethod.GET, requestEntity, String.class).getBody();
+        url = koneksiDB.URLORTHANC()+":"+koneksiDB.PORTORTHANC()+"/instances/"+studies_id+"/rendered?quality=20&height=200&width=190";
         return url;
     }
     
